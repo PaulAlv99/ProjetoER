d8c6796e699cb1b7081027ca24a2b1b9ea89e0b5
Paulo Alves <2120722@student.uma.pt>
2024-11-29 12:24:08 +0000

    registo e login

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.set('view engine', 'ejs');
-app.get('/registo', (req, res) => {
-  res.render("registoForm")
-})
-app.get('/login', (req, res) => {
-  res.render("logForm")
-})
+app.set("view engine", "ejs");
+app.use(express.static("src/public"));
+
+app.get("/registo", (req, res) => {
+  res.render("registoForm");
+});
+app.get("/login", (req, res) => {
+  res.render("logForm");
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
