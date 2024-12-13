b1f02df637aa392f1bcbc2e5ba09c4e9b9461975
Franciscoafonseca <129768120+Franciscoafonseca@users.noreply.github.com>
2024-12-13 04:09:14 +0000

    Pesquisa feito e estilizacao

--- controllers/pesquisa.js
+++ controllers/pesquisa.js
@@ -1,5 +1,52 @@
 const Item = require("../models/Item");
 
+// Definir as localidades e categorias fora da função
+const localidades = {
+  vianadocastelo: "Viana do Castelo",
+  porto: "Porto",
+  braga: "Braga",
+  lisboa: "Lisboa",
+  coimbra: "Coimbra",
+  faro: "Faro",
+  aveiro: "Aveiro",
+  leiria: "Leiria",
+  guarda: "Guarda",
+  evora: "Évora",
+  setubal: "Setúbal",
+  vila_real: "Vila Real",
+  viseu: "Viseu",
+  castelo_branco: "Castelo Branco",
+  braganca: "Bragança",
+  santarem: "Santarém",
+  beja: "Beja",
+  portalegre: "Portalegre",
+  madeira: "Madeira",
+  acores: "Açores",
+};
+
+const categorias = {
+  jardinagem: "Jardinagem",
+  tecnologia: "Tecnologia",
+  construcao: "Construção",
+  automoveis: "Automóveis",
+  educacao: "Educação",
+  saude: "Saúde",
+  moda: "Moda",
+  beleza: "Beleza",
+  alimentacao: "Alimentação",
+  turismo: "Turismo",
+  desporto: "Desporto",
+  cultura: "Cultura",
+  musica: "Música",
+  arte: "Arte",
+  imoveis: "Imóveis",
+  empregos: "Empregos",
+  servicos: "Serviços",
+  informatica: "Informática",
+  livros: "Livros",
+  animais: "Animais",
+};
+
 // Função para pesquisar itens
 const pesquisarItens = async (req, res) => {
   try {
@@ -17,13 +64,15 @@ const pesquisarItens = async (req, res) => {
     const resultados = await Item.find(query);
     console.log(resultados);
 
-    // Renderiza a página com os resultados encontrados
+    // Verifica e formata os dados de nome, localizacao e categoria
+    const nomeFormatado = nome === "" ? null : nome;
     const localizacaoFormatada = localizacao ? localidades[localizacao] : null;
     const categoriaFormatada = categoria ? categorias[categoria] : null;
 
+    // Renderiza a página com os resultados encontrados
     return res.render("resultadosPesquisa", {
       resultados,
-      nome,
+      nome: nomeFormatado,
       localizacao: localizacaoFormatada,
       categoria: categoriaFormatada,
       error: null,
@@ -32,6 +81,9 @@ const pesquisarItens = async (req, res) => {
     console.error("Erro ao realizar a pesquisa:", error);
     return res.render("resultadosPesquisa", {
       resultados: [],
+      nome: null,
+      localizacao: null,
+      categoria: null,
       error: "Ocorreu um erro ao realizar a pesquisa. Tente novamente.",
     });
   }
